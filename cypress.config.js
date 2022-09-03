75817bfd8a44b7eff2c209d98bfa65eb143c52be
Bjørn Gilstad <bjorn.gilstad@spk.no>
2022-09-03 11:44:22 +0200

    Cypress test

---
+++ cypress.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  e2e: {},
+  projectId: "4b7344",
+};
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,19 @@
+describe("skatteetaten.no home page", () => {
+  it("should change language", () => {
+    cy.visit("https://www.skatteetaten.no");
+    cy.get(
+      "html body.skatt.startpage.skatteetaten.is-desktop main div#main-content.skatt-body div.content div.container div.startpage-row div.row.row0 div.block.highfocusblock.col-lg-12.col-md-12.col-sm-12.col-xs-12 div div.focus-block.has-fill.has-image.image-left.image-medium.background-blue.mb-6 div.text h2.mb-5"
+    ).should("have.text", "Skatteoppgjør");
+    cy.get("#dropdownMenuLanguage").click();
+    cy.get("#lang-en").click();
+    cy.get(
+      "html body.skatt.startpage.skatteetaten.is-desktop main div#main-content.skatt-body div.content div.container div.startpage-row div.row.row0 div.block.highfocusblock.col-lg-12.col-md-12.col-sm-12.col-xs-12 div div.focus-block.has-fill.has-image.image-left.image-medium.background-blue.mb-6 div.text h2.mb-5"
+    ).should("have.text", "Tax assessment");
+  });
+
+  // Lag test her
+  // 1. Gå til siden "Ledige jobber"
+  // 2. Fyll inn "skattejurist" i søkefelt
+  // 3. Se at listen inneholder 3 elementer
+  it("should have 3 positions for skattejurist", () => {});
+});
